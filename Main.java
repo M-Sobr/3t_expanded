6f910d5e822d9303fce84696344665ca8d2ea0ad
Max Sobr <msob973@aucklanduni.ac.nz>
2025-07-11 10:57:42 +1200

    Added command line interface for choosing a starting board to play the game

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String... args) {
-        new Game3x3().run();
+        System.out.println("Enter an option to play a game of tic-tac-toe with:");
+        System.out.println("1) 3x3 game");
+        System.out.println("2) 4x4 game");
+        System.out.println("3) 9x9 game");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("> ");
+            String input = (scanner.nextLine()).trim();
+            if (input.equals("1")) { // 3x3 game
+                new Game3x3().run();
+                break;
+            } else if (input.equals("2")) { // 4x4 game
+                
+                break;
+            } else if (input.equals("3")) { // 9x9 game
+                
+                break;
+            }
+        }
+        scanner.close();
     }
 }
